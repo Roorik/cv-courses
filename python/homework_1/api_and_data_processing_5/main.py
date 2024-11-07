de8e7b1ba5cf4e65b5d8df6b048798367345d189
Roorik <ameno8dorime@gmail.com>
2024-11-08 04:59:59 +0500

    api weather

---
+++ python/homework_1/api_and_data_processing_5/main.py
@@ -0,0 +1,78 @@
+# Используйте внешний API для получения данных о текущей погоде в заданном городе и сохраните эти данные в файл.
+import json
+import logging
+import requests
+
+from pathlib import Path
+
+# определяем директорию под чтение и запись файлов, логов
+BASE_DIR = Path(__file__).parent
+output_dir = BASE_DIR.joinpath('weather.json')
+log_dir = BASE_DIR.joinpath('api_log.txt')
+
+# настраиваем логирование
+logging.basicConfig(filename=log_dir, level=logging.INFO, filemode='w')
+log = logging.getLogger(__name__)
+
+def get_coordinates(city):
+    # получаем координаты указанного города
+    try:
+        url = f'https://nominatim.openstreetmap.org/search?q={city}&format=json&limit=1'
+        headers = {'User-Agent':'Mozilla/5.0 (compatible; AcmeInc/1.0)'}
+        response = requests.get(url=url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            if data:
+                log.info('Данные от API геокодинга успешно получены')
+                return float(data[0]['lat']), float(data[0]['lon'])
+    except Exception as e:
+        log.error(f'Ошибка на этапе обращения к API геокодинга: {e}')
+    return None
+
+def get_weather(city):
+    # получаем данные о погоде
+    try:
+        lat, lon = get_coordinates(city)
+        url = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min'
+        response = requests.get(url)
+        if response.status_code == 200:
+            log.info('Данные от API погоды успешно получены')
+            return response.json()
+    except Exception as e:
+        log.error(f'Ошибка на этапе обращения к API погоды: {e}')
+    return None
+
+
+if __name__ == '__main__':
+    try:
+        city_name = input('Введите название города: ')
+        log.info(f'Указано название города: {city_name}')
+        weather_data = get_weather(city_name)
+        daily = weather_data.get('daily', []) 
+        time = daily.get('time')
+        temp_min = daily.get('temperature_2m_min', [])
+        temp_max = daily.get('temperature_2m_max', [])
+        
+        data = {
+            city_name: {
+                'time': time,
+                'temp_min': temp_min,
+                'temp_max': temp_max
+            }
+        }
+        
+        with open(output_dir, 'w') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+            log.info(f'Данные получены и записаны в файл {output_dir}')
+            log.info(f'Полученные данные: {daily}')
+        
+    except Exception as e:
+        log.error(f'Ошибка на этапе получения данных: {e}')
+        log.error(e)
+    
+    try:
+        ln = min(len(time), len(temp_min), len(temp_max))
+        for i in range(ln):
+            print(f'Погода {time[i]} ожидается в районе от {temp_min[i]} до {temp_max[i]}')
+    except Exception as e:
+        log.error(f'Ошибка на этапе перебора списков: {e}')
